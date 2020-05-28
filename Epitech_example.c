58d2a3aafff837f7120a3555db884e91b9352590
Léo Riberon-Piatyszek <leo.riberon-piatyszek@epitech.eu>
2020-05-28 12:00:20 +0200

    Update for read & write

--- Epitech_example.c
+++ Epitech_example.c
@@ -88,8 +88,6 @@ static ssize_t device_read(struct file *flip, char __user *buffer, size_t size, 
     printk(KERN_INFO "%s\n", buffer);
     if (len <= 0)
         return 0;
-    if (copy_to_user(msg_buffer, msg_buffer + *offset, len))
-        return -EFAULT;
     *offset += len;
     return len;
 }
